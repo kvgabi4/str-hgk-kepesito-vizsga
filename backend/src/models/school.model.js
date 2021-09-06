6c2ffd6309b76671ff767cb9cf590cb2380d0137
kvgabi4 <kvgabi4@gmail.com>
2021-09-06 13:05:26 +0200

    2. feladat

--- backend/src/models/school.model.js
+++ backend/src/models/school.model.js
@@ -1,3 +1,39 @@
 /**
  * Exercise: make School mongoose model
  */
+ const mongoose = require('mongoose');
+
+ const SchoolSchema = mongoose.Schema({
+     name: {
+         type: String,
+         required: true
+     },
+     city: {
+         type: String,
+         required: true
+     },
+     street: {
+         type: String,
+         required: true
+     },
+     zipcode: {
+         type: Number,
+         required: true
+     },
+     classroom: [{
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'Classroom'
+     }]
+ }, {
+     timestamps: true
+ });
+ 
+ module.exports = new mongoose.model('School', SchoolSchema, 'schools');
+
+
+ /* _id?:string;
+ name:string;
+ city: string;
+ street: string;
+ zipcode: number;
+ classrooms?: string[];  // references for classroom entities */
